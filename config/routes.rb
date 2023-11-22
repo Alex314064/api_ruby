7f36c66d9d17d3e68d9020489caf3969754605df
Alex314064 <135997704+Alex314064@users.noreply.github.com>
2023-11-22 10:22:46 +0100

    ok

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  devise_for :users,
+             controllers: {
+               sessions: 'users/sessions',
+               registrations: 'users/registrations'
+             }
+  resources :articles do
+    resources :comments
+  end
+  get '/member-data', to: 'members#show'
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
