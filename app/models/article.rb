f00625510a97e45c68d25490f3f648010b32a829
Alex314064 <135997704+Alex314064@users.noreply.github.com>
2023-11-22 10:22:33 +0100

    ok

--- app/models/article.rb
+++
@@ -1,6 +0,0 @@
-class Article < ApplicationRecord
-  validates :title, presence: true
-  validates :content, presence: true
-  has_many :comments
-  belongs_to :user
-end
--- db/migrate/20231121130710_add_private_to_articles.rb
+++
@@ -1,5 +0,0 @@
-class AddPrivateToArticles < ActiveRecord::Migration[7.1]
-  def change
-    add_column :articles, :private, :boolean
-  end
-end
